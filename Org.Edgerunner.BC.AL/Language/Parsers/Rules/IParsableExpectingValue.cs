4986f30726d1d41d6d0fda49f0a76dfd7cc0e3c2
Thaddeus Ryker <thad@edgerunner.org>
2023-11-05 16:37:48 -0500

    Added new interfaces

---
+++ Org.Edgerunner.BC.AL/Language/Parsers/Rules/IParsableExpectingValue.cs
@@ -0,0 +1,42 @@
+﻿#region MIT License
+// <copyright company = "Edgerunner.org" file = "IParsableExpectingValue.cs">
+// Copyright(c)  2023
+// </copyright>
+// The MIT License (MIT)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#endregion
+
+using Org.Edgerunner.BC.AL.Language.Tokens;
+using Org.Edgerunner.Language.Lexers;
+
+namespace Org.Edgerunner.BC.AL.Language.Parsers.Rules
+{
+   public interface IParsableExpectingValue
+   {
+      /// <summary>
+      /// Parses this rule from the token stream and expects to match the specified value.
+      /// </summary>
+      /// <param name="tokens">The token stream.</param>
+      /// <param name="context">The parser context.</param>
+      /// <param name="expectedValue">The expected value.</param>
+      /// <returns><c>true</c> if parsing was successful, <c>false</c> otherwise.</returns>
+      bool Parse(TokenStream<AlToken> tokens, AlParser context, string expectedValue);
+   }
+}
